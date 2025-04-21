ef15560df4085377ed12b0ee4f87c358ad4f5777
Rafael Bonotto <rafareisaba@outlook.com>
2025-04-21 20:47:40 -0300

    Criação da entidade

---
+++ app-mediatr/Entities/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace app_mediatr.Entities
+{
+    public class Cliente
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Idade { get; set; }
+        public bool Ativo { get; set; } = false;
+    }
+}
