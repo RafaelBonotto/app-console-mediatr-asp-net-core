ab97998c8c6e20d505d00dc0d85e3ea069914341
Rafael Bonotto <rafareisaba@outlook.com>
2025-04-27 20:44:07 -0300

    Adiciona DTO Produto

--- app-mediatr/DTOs/ClienteDTO.cs
+++ app-mediatr/DTOs/ClienteDTO.cs
@@ -2,6 +2,7 @@
 {
     public class ClienteDTO
     {
+        public long Id { get; set; }
         public string Nome { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public int Idade { get; set; }
---
+++ app-mediatr/DTOs/ProdutoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace app_mediatr.DTOs
+{
+    public class ProdutoDTO
+    {
+        public long Id { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        public double Preco { get; set; }
+        public bool Ativo { get; set; } = false;
+    }
+}
