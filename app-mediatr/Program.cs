ea5cd134106250e9fa3eb72f9152cc385be47380
Rafael Bonotto <rafareisaba@outlook.com>
2025-04-27 20:48:27 -0300

    Adiciona conversão do Produto Entidade para DTO

--- app-mediatr/Program.cs
+++ app-mediatr/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using app_mediatr.DTOs;
+using app_mediatr.Entities;
+using app_mediatr.Repositorys;
+
+Console.WriteLine("-------------- EXPLICITY OPERATOR: --------------");
+Console.WriteLine("Simulando uma chamada para repositório de Clientes... \n");
+
+var clienteRepository = new ClienteRepository();
+Cliente clienteEntidade = clienteRepository.Get();
+
+Console.WriteLine($"Cliente Entidade:");
+Console.WriteLine($"Cliente Id: {clienteEntidade.Id}");
+Console.WriteLine($"Cliente Nome: {clienteEntidade.Nome}\n");
+Console.WriteLine("Convertendo cliente entidade para cliente DTO... \n");
+
+ClienteDTO clienteDTO = (ClienteDTO)clienteEntidade; // -->> Necessário CAST Explicito para conversão funcionar
+
+Console.WriteLine($"Cliente DTO:");
+Console.WriteLine($"Cliente Id: {clienteDTO.Id}");
+Console.WriteLine($"Cliente Nome: {clienteDTO.Nome}\n\n");
+
+Console.WriteLine("-------------- IMPLICITY OPERATOR: --------------");
+Console.WriteLine("Simulando uma chamada para repositório de Produtos... \n");
+
+var produtoRepository = new ProdutoRepository();
+Produto produtoEntidade = produtoRepository.Get();
+
+Console.WriteLine($"Produto Entidade:");
+Console.WriteLine($"Produto Id: {produtoEntidade.Id}");
+Console.WriteLine($"Produto Descrição: {produtoEntidade.Descricao}\n");
+Console.WriteLine("Convertendo produto entidade para produto DTO... \n");
+
+ProdutoDTO produtoDTO = produtoEntidade; // -->> Não é necessário CAST Explicito
+
+Console.WriteLine($"Produto DTO:");
+Console.WriteLine($"Produto Id: {produtoDTO.Id}");
+Console.WriteLine($"Produto Descirção: {produtoDTO.Descricao}");
