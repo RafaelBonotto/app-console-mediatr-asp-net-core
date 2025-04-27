10e6949c7451524ab7ef1ff71f789233e26407cf
Rafael Bonotto <rafareisaba@outlook.com>
2025-04-27 20:47:41 -0300

    Adiciona Repositorio de Produto

---
+++ app-mediatr/Repositorys/ClienteRepository.cs
@@ -0,0 +1,24 @@
+﻿using app_mediatr.Entities;
+
+namespace app_mediatr.Repositorys
+{
+    /// <summary>
+    /// Essa classe é responsável pela simulação d um repositório que retorna uma entidade Cliente
+    /// </summary>
+    public class ClienteRepository
+    {
+        public Cliente? Cliente { get; set; }
+
+        public Cliente Get()
+        {
+            return new Cliente()
+            {
+                Id = 12578,
+                Nome = "Bruce Wayne",
+                Email = "brucewayne@email.com",
+                Idade = 30,
+                Ativo = true
+            };
+        }
+    }
+}
---
+++ app-mediatr/Repositorys/ProdutoRepository.cs
@@ -0,0 +1,23 @@
+﻿using app_mediatr.Entities;
+
+namespace app_mediatr.Repositorys
+{
+    /// <summary>
+    /// Essa classe é responsável pela simulação d um repositório que retorna uma entidade Cliente
+    /// </summary>
+    public class ProdutoRepository
+    {
+        public Produto? Produto { get; set; }
+
+        public Produto Get()
+        {
+            return new Produto()
+            {
+                Id = 1455,
+                Descricao = "Garrafa",
+                Preco = 18.99,
+                Ativo = true
+            };
+        }
+    }
+}
