90b0ab3af9c0266a0a323f0e0de20278502157ff
Rafael Bonotto <rafareisaba@outlook.com>
2025-04-27 20:47:09 -0300

    Adiciona entidade Produto utilizando Implicity operator para converter entidade em DTO

--- app-mediatr/Entities/Cliente.cs
+++ app-mediatr/Entities/Cliente.cs
@@ -1,10 +1,32 @@
-﻿namespace app_mediatr.Entities
+﻿using app_mediatr.DTOs;
+
+namespace app_mediatr.Entities
 {
+    /// <summary>
+    /// Essa classe utiliza como exemplo o explicity operator para converter a entidade em um DTO
+    /// </summary>
     public class Cliente
     {
+        public long Id { get; set; }
         public string Nome { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public int Idade { get; set; }
-        public bool Ativo { get; set; } = false;        
+        public bool Ativo { get; set; } = false;    
+        
+        /// <summary>
+        /// Metodo responsável pela conversão da entidade em um DTO
+        /// </summary>
+        /// <param name="entity"></param>
+        public static explicit operator ClienteDTO(Cliente entity)
+        {
+            return new ClienteDTO 
+            { 
+                Id = entity.Id,
+                Nome = entity.Nome, 
+                Email = entity.Email, 
+                Idade = entity.Idade, 
+                Ativo = entity.Ativo 
+            };
+        }
     }
 }
---
+++ app-mediatr/Entities/Produto.cs
@@ -0,0 +1,30 @@
+﻿using app_mediatr.DTOs;
+
+namespace app_mediatr.Entities
+{
+    /// <summary>
+    /// Essa classe utiliza como exemplo o implicity operator para converter a entidade em um DTO
+    /// </summary>
+    public class Produto
+    {
+        public long Id { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        public double Preco { get; set; }
+        public bool Ativo { get; set; } = false;
+
+        /// <summary>
+        /// Metodo responsável pela conversão da entidade em um DTO
+        /// </summary>
+        /// <param name="entity"></param>
+        public static implicit operator ProdutoDTO(Produto entity)
+        {
+            return new ProdutoDTO
+            {
+                Id = entity.Id,
+                Descricao = entity.Descricao,
+                Preco = entity.Preco,
+                Ativo = entity.Ativo
+            };
+        }
+    }
+}
